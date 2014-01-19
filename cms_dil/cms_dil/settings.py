639386aba00d67cb7492d89cf78aa6ef380d53ed
Sid Feygin <sid.feygin@berkeley.edu>
2014-01-18 17:18:40 -0800

    "lala"

--- cms_dil/cms_dil/settings.py
+++ cms_dil/cms_dil/settings.py
@@ -1,5 +1,11 @@
 # Django settings for cms_dil project.
 import os
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 gettext = lambda s: s
 PROJECT_PATH = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
 
